bf5a032947efc8acf3a42785b7314e1babdd11de
Mahmoud Khalid Shabaan Omar <mksomar200@gmail.com>
2025-03-27 01:08:53 +0200

    Add files via upload

---
+++ problem1/js/index.js
@@ -0,0 +1,3 @@
+// write a java script problem to print hello , and welcome sentence for the different users in the console =>
+console.log("Hello World!")
+console.log("Hello Everyone! , And Welcome To \"Frist Problem Solving With JS")
