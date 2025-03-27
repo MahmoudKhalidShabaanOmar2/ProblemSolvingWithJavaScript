d2d7e254f08a44d4832ca62071e8dd4e3fde6639
Mahmoud Khalid Shabaan Omar <mksomar200@gmail.com>
2025-03-27 05:00:01 +0200

    Add files via upload

---
+++ problem3/js/index.js
@@ -0,0 +1,8 @@
+// write a java script program to print all english alphabetic characters in the traingle shape , and you should put each english alphabetic character between double qoutation , and also should make a tab between different characters =>
+console.log("\"A\"")
+console.log("\"B\"\t\"C\"")
+console.log("\"D\"\t\"E\"\t\"F\"")
+console.log("\"G\"\t\"H\"\t\"I\"\t\"J\"")
+console.log("\"K\"\t\"L\"\t\"M\"\t\"N\"\t\"N\"")
+console.log("\"O\"\t\"P\"\t\"Q\"\t\"R\"\t\"S\"\t\"T\"")
+console.log("\"T\"\t\"U\"\t\"V\"\t\"\W\"\t\"X\"\t\"Y\"\t\"Z\"")
