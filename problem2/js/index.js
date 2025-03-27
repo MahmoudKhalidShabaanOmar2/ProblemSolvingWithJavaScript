8d1fa3d3045efc94b357789af9e90ffde3d50687
Mahmoud Khalid Shabaan Omar <mksomar200@gmail.com>
2025-03-27 04:37:51 +0200

    Add files via upload

---
+++ problem2/js/index.js
@@ -0,0 +1,9 @@
+// write a java script program to display all english aplhabetic characters in the traingle shape in the console :
+console.log("A")
+console.log("BC")
+console.log("DEF")
+console.log("GHIJ")
+console.log("KLMN")
+console.log("OPQR")
+console.log("STUVW")
+console.log("UVWXYZ")
