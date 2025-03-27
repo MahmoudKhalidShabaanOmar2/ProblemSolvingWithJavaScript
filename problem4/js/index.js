b32ad29dc2e4f44f2c59da153e5921c1bc1e9796
Mahmoud Khalid Shabaan Omar <mksomar200@gmail.com>
2025-03-27 05:20:38 +0200

    Add files via upload

---
+++ problem4/js/index.js
@@ -0,0 +1,12 @@
+// write a java script program to add different contents in the html document by using java script code , and then get the name , and age , and display in the console , and you should decleration variable =>
+document.getElementById("mainHeading") . innerHTML = "Hello EveryOne! , And Welcome To The Problem Solving With JS"
+var fristUserName = "mahmoud"
+var secondUserName = "khalid"
+var lastUserName = "shabaan"
+var fullUserName = fristUserName + "\t" + secondUserName + "\t" + lastUserName
+console.log("the frist name of the user is : ",fristUserName)
+console.log("the second name of the user is : ",secondUserName)
+console.log("the last name of the user is : ",lastUserName)
+console.log("the full name of the user is : ",fullUserName)
+var userAge = 22 
+console.log("the age of the user is = ",userAge," years")
