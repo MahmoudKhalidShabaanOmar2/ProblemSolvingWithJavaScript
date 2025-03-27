0e0c4dea4667546f5c3434d657d05ed4b5c1501d
Mahmoud Khalid Shabaan Omar <mksomar200@gmail.com>
2025-03-27 05:36:34 +0200

    Add files via upload

---
+++ problem5/js/index.js
@@ -0,0 +1,4 @@
+// write a java script program to allow number from the user , and then display the number for the user in the console : 
+document.getElementById("mainHeading") . innerHTML = "Please Enter The Integer Number"
+var number = window.prompt("Please Enter The Number Is = ")
+console.log("the number is = ",number)
